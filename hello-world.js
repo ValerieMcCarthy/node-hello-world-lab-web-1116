8ae3181f6855ceb44e1113727941c56316159ed3
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-06-27 17:04:42 -0400

    hi world

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+var helloWorld = function () {
+    return 'Hello World'
+}
+
+module.exports = helloWorld
+
+
+// This is the content of `hello-world.js` which will pass the tests:
+
+// var helloWorld  = function() {
+  // return 'Hello World'
+// }
+// module.exports = helloWorld // for require('./hello-world.js')
